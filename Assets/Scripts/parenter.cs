66bc26d4ef5a46713ad76ba1ac4a9bad2f6e03e4
jsunyerdev <jordi.lopez.sune@estudiantat.upc.edu>
2022-04-24 22:19:20 +0200

    Fix Plataforma puzzle 6

---
+++ Assets/Scripts/parenter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Parenter : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider _is)
+    {
+        if (_is.gameObject == player)
+        {
+            player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider _is)
+    {
+        if (_is.gameObject == player)
+        {
+            player.transform.parent = null;
+        }
+    }
+
+}
