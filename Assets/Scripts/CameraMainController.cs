1494cb9dc48593eb2ff6b4e61eab22514c597dff
jsunyerdev <jordi.lopez.sune@estudiantat.upc.edu>
2022-06-02 01:01:52 +0200

    Millora rotació càmera Player II

--- Assets/Scripts/CameraMainController.cs
+++ Assets/Scripts/CameraMainController.cs
@@ -9,10 +9,15 @@ public class CameraMainController : MonoBehaviour
     private CinemachineFramingTransposer _framingTransposer;
     private CinemachineBasicMultiChannelPerlin _multiChannelPerlin;
 
+    public float _defaultAreaCameraDistance = 8;
+    public float _defaultVerticalArmLength = 0.66f;
+
     void Start()
     {
         _framingTransposer = _vcam.GetCinemachineComponent<CinemachineFramingTransposer>();
         _multiChannelPerlin = _vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        _defaultAreaCameraDistance = _framingTransposer.m_CameraDistance;
+        _defaultVerticalArmLength = _framingTransposer.m_ScreenY;
     }
 
     public void CameraMovementEnter(CameraZoomController camera)
@@ -24,7 +29,7 @@ public class CameraMainController : MonoBehaviour
     public void CameraMovementExit(CameraZoomController camera)
     {
         StopAllCoroutines();
-        StartCoroutine(DoCameraMovement(camera, camera._defaultAreaCameraDistance, camera._defaultVerticalArmLength, camera._defaultAreaCameraRotation, camera._defaultAreaNoiseFrequencyGain, camera._defaultAreaAmplitudGain, camera._exitTransitionSeconds));
+        StartCoroutine(DoCameraMovement(camera, _defaultAreaCameraDistance, _defaultVerticalArmLength, camera._defaultAreaCameraRotation, camera._defaultAreaNoiseFrequencyGain, camera._defaultAreaAmplitudGain, camera._exitTransitionSeconds));
     }
 
     private IEnumerator DoCameraMovement(CameraZoomController camera, float distance, float verticalArmLength, Vector3 cameraRotation, float noiseFrequencyGain, float noiseAmplitudGain, float duration)
@@ -48,11 +53,11 @@ public class CameraMainController : MonoBehaviour
         {
             float x = Mathf.Clamp01(t / duration);
             float f = 3 * Mathf.Pow(x, 2) - 2 * Mathf.Pow(x, 3);
-            camera._framingTransposer.m_CameraDistance = Mathf.Lerp(startDistance, endDistance, f);
-            camera._framingTransposer.m_ScreenY = Mathf.Lerp(startVerticalArmLength, endVerticalArmLength, f);
-            camera._vcam.gameObject.transform.rotation = Quaternion.Lerp(startRotation, endRotation, f);
-            camera._multiChannelPerlin.m_FrequencyGain = Mathf.Lerp(startNoiseFrequencyGain, endNoiseFrequencyGain, f);
-            camera._multiChannelPerlin.m_AmplitudeGain = Mathf.Lerp(startNoiseAmplitudGain, endNoiseAmplitudGain, f);
+            _framingTransposer.m_CameraDistance = Mathf.Lerp(startDistance, endDistance, f);
+            _framingTransposer.m_ScreenY = Mathf.Lerp(startVerticalArmLength, endVerticalArmLength, f);
+            _vcam.gameObject.transform.rotation = Quaternion.Lerp(startRotation, endRotation, f);
+            _multiChannelPerlin.m_FrequencyGain = Mathf.Lerp(startNoiseFrequencyGain, endNoiseFrequencyGain, f);
+            _multiChannelPerlin.m_AmplitudeGain = Mathf.Lerp(startNoiseAmplitudGain, endNoiseAmplitudGain, f);
             yield return null;
         }
     }
--- Assets/Scripts/CameraZoomController.cs
+++ Assets/Scripts/CameraZoomController.cs
@@ -17,9 +17,6 @@ public class CameraZoomController : MonoBehaviour
 
     [SerializeField]
     public float _areaAmplitudGain = 2f;
-
-    public float _defaultAreaCameraDistance = 8;
-    public float _defaultVerticalArmLength = 0.66f;
     public Vector3 _defaultAreaCameraRotation = Vector3.zero;
     public float _defaultAreaNoiseFrequencyGain = 0.3f;
     public float _defaultAreaAmplitudGain = 0.5f;
@@ -36,9 +33,7 @@ public class CameraZoomController : MonoBehaviour
     [SerializeField]
     public float _exitTransitionSeconds = 2;
 
-    public CinemachineVirtualCamera _vcam;
     public CinemachineFramingTransposer _framingTransposer;
-    public CinemachineBasicMultiChannelPerlin _multiChannelPerlin;
 
     [SerializeField]
     public bool _justOnce;
@@ -47,59 +42,11 @@ public class CameraZoomController : MonoBehaviour
     [SerializeField]
     public bool _useCamaraRotation;
 
-
-    void Start()
-    {
-        _framingTransposer = _vcam.GetCinemachineComponent<CinemachineFramingTransposer>();
-        _multiChannelPerlin = _vcam.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-        _defaultAreaCameraDistance = _framingTransposer.m_CameraDistance;
-        _defaultVerticalArmLength = _framingTransposer.m_ScreenY;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private IEnumerator SetCameraDistance(float distance, float verticalArmLength, Vector3 cameraRotation, float noiseFrequencyGain, float noiseAmplitudGain, float duration)
-    {
-        float startDistance = _framingTransposer.m_CameraDistance;
-        float endDistance = distance;
-
-        float startVerticalArmLength = _framingTransposer.m_ScreenY;
-        float endVerticalArmLength = verticalArmLength;
-
-        float startNoiseFrequencyGain = _multiChannelPerlin.m_FrequencyGain;
-        float endNoiseFrequencyGain = noiseFrequencyGain;
-
-        float startNoiseAmplitudGain = _multiChannelPerlin.m_AmplitudeGain;
-        float endNoiseAmplitudGain = noiseAmplitudGain;
-
-        Quaternion startRotation = _vcam.gameObject.transform.rotation;
-        Quaternion endRotation = Quaternion.Euler(startRotation.x + cameraRotation.x, startRotation.y + cameraRotation.y, startRotation.z + cameraRotation.z);
-
-        for (float t = 0; t <= duration; t += Time.deltaTime)
-        {
-            float x = Mathf.Clamp01(t / duration);
-            float f = 3 * Mathf.Pow(x, 2) - 2 * Mathf.Pow(x, 3);
-            _framingTransposer.m_CameraDistance = Mathf.Lerp(startDistance, endDistance, f);
-            _framingTransposer.m_ScreenY = Mathf.Lerp(startVerticalArmLength, endVerticalArmLength, f);
-            _vcam.gameObject.transform.rotation = Quaternion.Lerp(startRotation, endRotation, f);
-            _multiChannelPerlin.m_FrequencyGain = Mathf.Lerp(startNoiseFrequencyGain, endNoiseFrequencyGain, f);
-            _multiChannelPerlin.m_AmplitudeGain = Mathf.Lerp(startNoiseAmplitudGain, endNoiseAmplitudGain, f);
-            yield return null;
-        }
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (_enterTransition) {
             if (other.tag == "Player" && (!_justOnce || (_justOnce && !_entered))) {
                 other.GetComponent<PlayerController>()._useCameraRotation = _useCamaraRotation;
-                //Debug.Log("Player Enter Camera Zone");
-                //StopAllCoroutines();
-                //StartCoroutine(SetCameraDistance(_areaCameraDistance, _areaCameraVerticalArmLength, _areaCameraRotation, _areaNoiseFrequencyGain, _areaAmplitudGain, _enterTransitionSeconds));
                 _entered = true;
                 SendMessageUpwards("CameraMovementEnter", this);
             }
@@ -113,9 +60,6 @@ public class CameraZoomController : MonoBehaviour
             if (other.tag == "Player")
             {
                 BroadcastMessage("CameraMovementExit", this);
-                //Debug.Log("Player Exit Camera Zone");
-                //StopAllCoroutines();
-                //StartCoroutine(SetCameraDistance(_defaultAreaCameraDistance, _defaultVerticalArmLength, _defaultAreaCameraRotation, _defaultAreaNoiseFrequencyGain, _defaultAreaAmplitudGain, _exitTransitionSeconds));
             }
         }
     }
