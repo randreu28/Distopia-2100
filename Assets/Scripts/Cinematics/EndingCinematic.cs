5dc7e4c661b5e79b2ab81140321d207cd1ee58d1
randreu28 <randreu28@gmail.com>
2022-06-02 19:16:31 +0200

    Cinematica final
    Necesitaré ayuda con una cosilla @jsunyerdev

---
+++ Assets/Scripts/Cinematics/EndingCinematic.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingCinematic : MonoBehaviour
+{
+    public GameObject EndingCredits;
+
+    private Fader _fader;
+
+    void OnTriggerEnter(Collider Player)
+    {
+        if(Player.gameObject.tag == "Player")
+        {
+            FadeOutEnd();
+            //Me mata si hago esto y no sé porqué! @Jordi
+            
+            /* _fader = Player.GetComponent<Fader>();
+           _fader.FadeOut(); */
+        }
+    }
+
+    public void FadeOutEnd()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0;
+        EndingCredits.SetActive(true);
+    }
+}
--- Assets/Scripts/Respawn System/Menu.cs
+++ Assets/Scripts/Respawn System/Menu.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
     public GameObject Player;
-
     private RespawnSystem RespawnSystem;
 
     private Fader _fader;
@@ -23,6 +23,10 @@ public class Menu : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         Time.timeScale = 1;
+    }
+
+    public void MainMenuButton() {
+        SceneManager.LoadScene("Menu");
     }
 
     public void ExitButton() {
