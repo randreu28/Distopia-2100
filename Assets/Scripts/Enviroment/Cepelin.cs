e74017c44b9948814b9bc086ac237ce0bcbe7bef
randreu28 <randreu28@gmail.com>
2022-06-05 14:26:32 +0200

    Reworked cepeling v1
    WIP

--- Assets/Scripts/Enviroment/Cepelin.cs
+++ Assets/Scripts/Enviroment/Cepelin.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class Cepelin : MonoBehaviour
 {
-    public Vector3 distance;
-    public float cycleTime;
-    public float waitTime = 1;
+    [Header ("Puntos a recorrer")]
+    public Transform[] points;
+    [Header ("Tiempo de interpolación de la posición entre punto y punto")]
+    public float timePosition;
+    [Header ("Tiempo de interpolación de la rotación entre punto y punto")]
+    public float timeRotation;
+    [Header ("Tiempo de espera entre punto y punto")]
+    public float timeWait = 0f;
 
+    private Transform currentTransform;
 
-    private bool isMoving = false;
-    private Vector3 _startPosition;
     void Start()
     {
-        _startPosition = transform.position;
+        currentTransform = this.gameObject.transform;
         StartCoroutine(Loop());
     }
 
     IEnumerator Loop()
     {
-        yield return StartCoroutine(LerpPosition(_startPosition, _startPosition + distance, cycleTime));
-        yield return StartCoroutine(wait(waitTime));
-        yield return StartCoroutine(LerpPosition(_startPosition + distance, _startPosition, cycleTime));
-        yield return StartCoroutine(wait(waitTime));
-        yield return StartCoroutine(Loop());
+        for(int i = 0; i <= points.Length; i++)
+        {
+            yield return currentTransform = this.gameObject.transform;
+            yield return StartCoroutine(LerpPosition(currentTransform.position, points[i].position, timePosition));
+            yield return StartCoroutine(LerpRotation(currentTransform.rotation, points[i].rotation, timeRotation));
+            yield return StartCoroutine(wait(timeWait));
+        }
+        //yield return StartCoroutine(Loop());
     }
 
     IEnumerator wait(float time)
@@ -37,11 +44,25 @@ public class Cepelin : MonoBehaviour
         while (time < duration)
         {
             float t = time / duration;
-            t = t * t * (3f - 2f * t);
+            //t = t * t * (3f - 2f * t);
             transform.position = Vector3.Lerp(startPosition, targetPosition, t);
             time += Time.deltaTime;
             yield return null;
         }
         transform.position = targetPosition;
+    }
+
+    IEnumerator LerpRotation(Quaternion startRotation, Quaternion endRotation, float duration)
+    {
+        float time = 0;
+        while (time < duration)
+        {
+            float t = time / duration;
+            //t = t * t * (3f - 2f * t);
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, t);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.rotation = endRotation;
     }
 }
