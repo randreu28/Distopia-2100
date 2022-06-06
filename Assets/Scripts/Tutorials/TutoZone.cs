02a0e0865f9d2e653dd54ee00b8cf692776c359f
randreu28 <randreu28@gmail.com>
2022-06-06 12:54:30 +0200

    Reformas lvl 1
    Colliders, muertes por ascensor, shader electricidad y un intento de script de tutos

---
+++ Assets/Scripts/Tutorials/TutoZone.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutoZone : MonoBehaviour
+{
+    public GameObject TutorialCanvas;
+    public float fadeTime = 2;
+
+    void Awake() //Para asegurarse que todas las imagenes al iniciar el juego tienen alpha 0
+    {
+        int children = TutorialCanvas.transform.childCount;
+        for(int i = 0; i < children; i++)
+        {
+            if(TutorialCanvas.transform.GetChild(i).TryGetComponent(out Image image))
+            {
+                Color temp = image.color;
+                temp.a = 0f;
+                image.color = temp; 
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            int children = TutorialCanvas.transform.childCount;
+            for(int i = 0; i < children; i++)
+            {
+                if(TutorialCanvas.transform.GetChild(i).TryGetComponent(out Image image))
+                {
+                    //image.color = new Color(image.color.r, image.color.g, image.color.b, 255);
+                    StartCoroutine(LerpAlpha(image, 255, fadeTime));
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            int children = TutorialCanvas.transform.childCount;
+            for(int i = 0; i < children; i++)
+            {
+                if(TutorialCanvas.transform.GetChild(i).TryGetComponent(out Image image))
+                {
+                    //image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+                    StartCoroutine(LerpAlpha(image, 0, fadeTime));
+                }
+            }
+        }
+    }
+
+    IEnumerator LerpAlpha(Image image, float endValue, float duration)
+    {
+        float time = 0;
+        Color startValue = image.color;
+        while (time < duration)
+        {
+            float newAlpha = Mathf.Lerp(startValue.a, endValue, time / duration);
+            image.color = new Color(startValue.r, startValue.g, startValue.b, newAlpha);
+            Debug.Log(image.color.a);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        Debug.Log(image.color.a);
+        image.color = new Color(startValue.r, startValue.g, startValue.b, endValue);
+    }
+}
