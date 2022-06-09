57d2e8146155ce155f159d5ed4b837615f31cd75
randreu28 <randreu28@gmail.com>
2022-06-09 19:58:00 +0200

    Menu SFX & bug Fix
    Ahora hay un sonido al rollover de los botones, y he añadido el lvl1 en los buildSettings

--- Assets/Scripts/MenuPrincipal.cs
+++ Assets/Scripts/MenuPrincipal.cs
@@ -2,11 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class MenuPrincipal : MonoBehaviour
+public class MenuPrincipal : MonoBehaviour, IPointerEnterHandler
 {
     public string Scene;
+    
+    public AudioClip AudioClip;
+    [Range(0, 1)]
+    public float volume = 1f;
+    private AudioSource SFX;
+
+    void Awake()
+    {
+        SFX = gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+        SFX.clip = AudioClip;
+        SFX.volume = volume;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        SFX.Play();
+    }
 
     public void PlayButton() {
         Cursor.lockState = CursorLockMode.Locked;
